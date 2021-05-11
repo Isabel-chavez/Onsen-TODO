d68e905d4fc507b776bdc287807b4bab25b8fbeb
Isabel-chavez <73866172+Isabel-chavez@users.noreply.github.com>
2021-05-10 23:00:09 -0700

    web storage
    Se agrego almacenamiento local

--- js/app.js
+++ js/app.js
@@ -4,10 +4,46 @@ window.myApp = {};
 document.addEventListener('init', function(event) {
   var page = event.target;
 
+   console.log("page id:"+page.id);
+
+   // borramos del cache
+    borrar_cache();
+
   // Each page calls its own initialization controller.
   if (myApp.controllers.hasOwnProperty(page.id)) {
     myApp.controllers[page.id](page);
   }
+
+ /* ************** ANTES DE LLENAR LA LISTA DEBO SACAR LOS VALORES DEL STORAGE Y ASIGNARLOS A LA LISTA #pendingTasksPage     ************************************************/
+
+ /*
+ if (localStorage.getItem("eltitulo")) {
+    let recuperado= localStorage.getItem("eltitulo");
+    console.log("el cache: "+recuperado);
+ 
+    
+    myApp.services.tasks.create(
+    {
+      title: recuperado,
+     /* category: page.querySelector('#category-input').value,
+        description: page.querySelector('#description-input').value,
+       highlight: page.querySelector('#highlight-input').checked,
+       urgent: page.querySelector('#urgent-input').checked
+     }
+     
+  );
+ }
+*/
+   //Ahora  obtenenos los valores almacenados localmente
+            let v_titulo= localStorage.getItem("eltitulo");                 console.log("v_titulo:"+v_titulo);
+            let v_categoria=localStorage.getItem("la_categoria");           console.log("v_categoria:"+v_categoria);
+            let v_descripcion=localStorage.getItem("la_descripcion");       console.log("v_descripcion:"+v_descripcion);
+            let v_highlight=localStorage.getItem("highlight");              console.log("v_hightlight:"+v_highlight);
+            let v_urgente=localStorage.getItem("urgente");                  console.log("v_urgente:"+v_urgente);
+    
+
+          
+ /* ************************************************************* */
 
   // Fill the lists with initial data when the pages we need are ready.
   // This only happens once at the beginning of the app.
@@ -16,9 +52,24 @@ document.addEventListener('init', function(event) {
       && document.querySelector('#pendingTasksPage')
       && !document.querySelector('#pendingTasksPage ons-list-item')
     ) {
-      myApp.services.fixtures.forEach(function(data) {
-        myApp.services.tasks.create(data);
+         myApp.services.fixtures.forEach(function(data) {
+         myApp.services.tasks.create(data);
       });
     }
   }
 });
+
+// Borramos el cache
+function borrar_cache(){
+
+  
+  if (localStorage.getItem("eltitulo")) {
+    localStorage.removeItem("eltitulo");
+    localStorage.removeItem("la_categoria");
+    localStorage.removeItem("la_descripcion");
+    localStorage.removeItem("highlight");
+    localStorage.removeItem("urgente");
+  }
+  
+
+}
--- js/controllers.js
+++ js/controllers.js
@@ -11,9 +11,11 @@ myApp.controllers = {
     // Set button functionality to open/close the menu.
     page.querySelector('[component="button/menu"]').onclick = function() {
       document.querySelector('#mySplitter').left.toggle();
+      console.log("la page;"+page);
     };
 
     // Set button functionality to push 'new_task.html' page.
+    // Cuanso se presiona el boton new-task
     Array.prototype.forEach.call(page.querySelectorAll('[component="button/new-task"]'), function(element) {
       element.onclick = function() {
         document.querySelector('#myNavigator').pushPage('html/new_task.html');
@@ -43,26 +45,67 @@ myApp.controllers = {
   ////////////////////////////
   newTaskPage: function(page) {
     // Set button functionality to save a new task.
+    // pone el botton ADD New task
     Array.prototype.forEach.call(page.querySelectorAll('[component="button/save-task"]'), function(element) {
       element.onclick = function() {
-        var newTitle = page.querySelector('#title-input').value;
+        // Aquí sacamos 
+        var newTitle = page.querySelector('#title-input').value; console.log("title-input="+ newTitle);
 
+        // SI EXISTE UNA NUEVA TAREA TASK SE VA A CREAR
         if (newTitle) {
+          console.log("si existe una nueva tarea capturada");
+            //* ***************** AQUI ES DONDE DEBE PONER EL WEBSTORAGE *******************************************************/
+
+        
+        
+            // seteamos los valores del nuevo elemento
+            localStorage.setItem("eltitulo", newTitle);  
+            localStorage.setItem("la_categoria",page.querySelector('#category-input').value);
+            localStorage.setItem("la_descripcion",page.querySelector('#description-input').value);
+            localStorage.setItem("highlight",page.querySelector('#highlight-input').checked);
+            localStorage.setItem("urgente",page.querySelector('#urgent-input').checked);
+
+            //Ahora  obtenenos los valores almacenados localmente
+            let v_titulo= localStorage.getItem("eltitulo");                 console.log("v_titulo:"+v_titulo);
+            let v_categoria=localStorage.getItem("la_categoria");           console.log("v_categoria:"+v_categoria);
+            let v_descripcion=localStorage.getItem("la_descripcion");       console.log("v_descripcion:"+v_descripcion);
+            let v_highlight=localStorage.getItem("highlight");              console.log("v_hightlight:"+v_highlight);
+            let v_urgente=localStorage.getItem("urgente");                  console.log("v_urgente:"+v_urgente);
+    
+
+          /*   *****************END *****************************************************************************************/
+
+
+          
           // If input title is not empty, create a new task.
+          // Si el input del titulo no esta vacio ENTONCES se crea la nueva tarea en ala lista
           myApp.services.tasks.create(
-            {
+           /* {
               title: newTitle,
               category: page.querySelector('#category-input').value,
               description: page.querySelector('#description-input').value,
               highlight: page.querySelector('#highlight-input').checked,
               urgent: page.querySelector('#urgent-input').checked
             }
+              */
+
+            {
+              title: v_titulo,
+              category: v_categoria,
+              description: v_descripcion,
+              highlight: v_highlight,
+              urgent: v_urgente
+            }
+
+
           );
 
           // Set selected category to 'All', refresh and pop page.
           document.querySelector('#default-category-list ons-list-item ons-radio').checked = true;
           document.querySelector('#default-category-list ons-list-item').updateCategoryView();
           document.querySelector('#myNavigator').popPage();
+
+         
 
         } else {
           // Show alert if the input title is empty.
@@ -86,6 +129,14 @@ myApp.controllers = {
     page.querySelector('#highlight-input').checked = element.data.highlight;
     page.querySelector('#urgent-input').checked = element.data.urgent;
 
+
+    /* ***************AQUI DEBO SACAR LOS DATOS ALMACENDOS CON EL LOCALSTORAGE*********    */
+
+    // if (!localStorage.getItem(URL)) {
+     //  localStorage.setItem(URL, 'https://pokeapi.co/api/v2/pokemon');
+    // }
+
+
     // Set button functionality to save an existing task.
     page.querySelector('[component="button/save-task"]').onclick = function() {
       var newTitle = page.querySelector('#title-input').value;
@@ -100,15 +151,36 @@ myApp.controllers = {
           }
         ).then(function(buttonIndex) {
           if (buttonIndex === 1) {
+
+             //Ahora  obtenenos los valores almacenados localmente
+             let v_titulo= localStorage.getItem("eltitulo");                 console.log("v_titulo:"+v_titulo);
+             let v_categoria=localStorage.getItem("la_categoria");           console.log("v_categoria:"+v_categoria);
+             let v_descripcion=localStorage.getItem("la_descripcion");       console.log("v_descripcion:"+v_descripcion);
+             let v_highlight=localStorage.getItem("highlight");              console.log("v_hightlight:"+v_highlight);
+             let v_urgente=localStorage.getItem("urgente");                  console.log("v_urgente:"+v_urgente);
+     
+
+
             // If 'Save' button was pressed, overwrite the task.
+            // SI EL BOTON GRABAR FUE presionado, se sobreescribe el task
             myApp.services.tasks.update(element,
-              {
+             /* {
                 title: newTitle,
                 category: page.querySelector('#category-input').value,
                 description: page.querySelector('#description-input').value,
                 ugent: element.data.urgent,
                 highlight: page.querySelector('#highlight-input').checked
-              }
+              } */
+
+              {
+                title: v_titulo,
+                category: v_categoria,
+                description: v_descripcion,
+                urgent: v_urgente,
+                highlight: v_highlight
+              } 
+
+
             );
 
             // Set selected category to 'All', refresh and pop page.
